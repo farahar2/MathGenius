921cafeee845c83f70b29e6edced0d106229c113
farahar2 <farahafouzar@gmail.com>
2026-08-03 11:19:03 -0700

    add chapitre API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChapitreController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,3 +11,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 });
+
+Route::apiResource('chapitres', ChapitreController::class);
